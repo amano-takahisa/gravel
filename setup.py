c555e5a5ef9aa3fea4fe046d3bb5a12247d9bcb4
Taka <amano.takahisa@gmail.com>
2022-03-22 23:56:53 +0100

    fix typo

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 requirements = [
-    'GDAL>=3.4.2'
+    'GDAL>=3.4.2',
     'numpy>=1.22.3',
     'fiona>=1.8.21',
     'pyproj>=3.3.0',
