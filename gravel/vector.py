57e6478da82d8f0441e151c3cf706b315f32e6b9
Taka <amano.takahisa@gmail.com>
2022-03-22 19:53:10 +0100

    add type annotations

--- gravel/vector.py
+++ gravel/vector.py
@@ -1,17 +1,20 @@
+from typing import Tuple
+import geopandas as gpd
+from pyproj.crs import CRS
 class Vector:
     def __init__(self):
         pass
 
-    def to_gdf(self):
+    def to_gdf(self) -> gpd.GeoDataFrame:
         """Returns vector as a geopandas.GeoDataFrame object"""
         pass
 
     @property
-    def crs(self):
+    def crs(self) -> CRS:
         """Returns vector's CRS as a pyproj.crs.CRS object"""
         pass
 
     @property
-    def shape(self):
+    def shape(self) -> Tuple[int, int]:
         """Return a tuple representing the dimensionality of the DataFrame."""
         pass
