9c6716e83490e9901007d96d87a6622e96ffac7c
Taka <amano.takahisa@gmail.com>
2023-01-06 23:20:20 +0100

    use sphinx extensions to generate api docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,5 @@
+import collections
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -14,14 +16,40 @@ release = "0.1.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+]
 
+# Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# These patterns also affect html_static_path and html_extra_path
 exclude_patterns = []
 
+# A list of ignored prefixes for module index sorting.
+modindex_common_prefix = ["gravel."]
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+html_theme = "sphinx_rtd_theme"
 
-html_theme = "alabaster"
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+# -- Options for extensions -------------------------------------------------
+autodoc_default_options = {
+    "members": True,
+    "member-order": "bysource",
+    "special-members": True,
+    "undoc-members": True,
+    "special-members": "__init__",
+    "noindex": True,
+}
